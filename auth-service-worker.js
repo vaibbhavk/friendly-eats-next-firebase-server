ef0287a16600b23db874f4705a8976fc003721bf
Vaibhav Kesharwani <vaibhav.vk2128@gmail.com>
2025-03-02 11:18:07 +0530

    updated auth-service-worker

--- auth-service-worker.js
+++ auth-service-worker.js
@@ -5,16 +5,20 @@ import { getInstallations, getToken } from "firebase/installations";
 // this is set during install
 let firebaseConfig;
 
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   // extract firebase config from query string
-  const serializedFirebaseConfig = new URL(location).searchParams.get('firebaseConfig');
-  
+  const serializedFirebaseConfig = new URL(location).searchParams.get(
+    "firebaseConfig"
+  );
+
   if (!serializedFirebaseConfig) {
-    throw new Error('Firebase Config object not found in service worker query string.');
+    throw new Error(
+      "Firebase Config object not found in service worker query string."
+    );
   }
-  
+
   firebaseConfig = JSON.parse(serializedFirebaseConfig);
-  console.log("Service worker installed with Firebase config", firebaseConfig);
+  console.log("Service worker installed successfully!");
 });
 
 self.addEventListener("fetch", (event) => {
